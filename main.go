5b146abb6bd6890571d284966fa2da59df9eba2a
saufiroja <saufiroja08@gmail.com>
2022-10-26 15:23:34 +0700

    refactor: add port from env

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"graphql/blog-go-graphql/infrastructure/server"
 	"log"
+	"os"
 )
 
 func main() {
 	app := server.InitServer()
+	port := os.Getenv("PORT")
 
-	err := app.Listen(":8080")
+	err := app.Listen(":" + port)
 	if err != nil {
 		log.Panic(err)
 	}
