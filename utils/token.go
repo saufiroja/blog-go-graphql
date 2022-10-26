b327aedc15da807dbd404f200be65d5b851d42f9
saufiroja <saufiroja08@gmail.com>
2022-10-26 11:18:13 +0700

    feat: add jwt private key

--- utils/token.go
+++ utils/token.go
@@ -1,19 +1,23 @@
 package utils
 
 import (
+	"fmt"
+	"os"
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
 )
 
 func GenerateAccessToken(id, email string) (string, error) {
+	secret := os.Getenv("JWT_PRIVATE_KEY")
+	fmt.Println(secret)
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"id":    id,
 		"email": email,
 		"exp":   time.Now().Add(time.Hour * 24).Unix(),
 	})
 
-	tokenString, err := token.SignedString([]byte("secret"))
+	tokenString, err := token.SignedString([]byte(secret))
 	if err != nil {
 		return "", err
 	}
