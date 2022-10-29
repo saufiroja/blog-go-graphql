cc55c263d561ab2afe61770a2f8c9ce8e1bf72e0
saufiroja <saufiroja08@gmail.com>
2022-10-29 12:29:05 +0700

    feat: add like entity

---
+++ entity/like.entity.go
@@ -0,0 +1,20 @@
+package entity
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type Like struct {
+	ID        string `json:"id" gorm:"primaryKey"`
+	ArticleID string `json:"article_id" gorm:"not null"`
+	UserID    string `json:"user_id" gorm:"not null"`
+	CreatedAt int64  `json:"created_at" gorm:"autoCreateTime:milli"`
+	UpdatedAt int64  `json:"updated_at" gorm:"autoUpdateTime:milli"`
+	DeletedAt int64  `json:"deleted_at" gorm:"index"`
+}
+
+func (l *Like) BeforeCreate(tx *gorm.DB) (err error) {
+	l.ID = uuid.New().String()
+	return
+}
