2953f8b182f36fd1ef7df816c5f793d05f2decbd
saufiroja <saufiroja08@gmail.com>
2022-10-29 11:34:14 +0700

    feat: add comment in article

--- entity/article.entity.go
+++ entity/article.entity.go
@@ -6,17 +6,18 @@ import (
 )
 
 type Article struct {
-	ID        string `json:"id" gorm:"primaryKey"`
-	PhotoURL  string `json:"photo_url" gorm:"not null"`
-	Title     string `json:"title" gorm:"not null"`
-	Body      string `json:"body" gorm:"not null"`
-	Category  string `json:"category" gorm:"not null"`
-	Likes     int    `json:"likes" gorm:"not null"`
-	Dislikes  int    `json:"dislikes" gorm:"not null"`
-	UserID    string `json:"user_id" gorm:"not null"`
-	CreatedAt int64  `json:"created_at" gorm:"autoCreateTime:milli"`
-	UpdatedAt int64  `json:"updated_at" gorm:"autoUpdateTime:milli"`
-	DeletedAt int64  `json:"deleted_at" gorm:"index"`
+	ID        string    `json:"id" gorm:"primaryKey"`
+	PhotoURL  string    `json:"photo_url" gorm:"not null"`
+	Title     string    `json:"title" gorm:"not null"`
+	Body      string    `json:"body" gorm:"not null"`
+	Category  string    `json:"category" gorm:"not null"`
+	Likes     int       `json:"likes" gorm:"not null"`
+	Dislikes  int       `json:"dislikes" gorm:"not null"`
+	UserID    string    `json:"user_id" gorm:"not null"`
+	Comments  []Comment `json:"comments" gorm:"foreignKey:ArticleID"`
+	CreatedAt int64     `json:"created_at" gorm:"autoCreateTime:milli"`
+	UpdatedAt int64     `json:"updated_at" gorm:"autoUpdateTime:milli"`
+	DeletedAt int64     `json:"deleted_at" gorm:"index"`
 }
 
 func (a *Article) BeforeCreate(tx *gorm.DB) (err error) {
