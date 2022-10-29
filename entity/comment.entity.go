0629fc540b4f06d321c29938d3614bca3bc07067
saufiroja <saufiroja08@gmail.com>
2022-10-29 11:31:28 +0700

    feat: add entity database

--- entity/comment.entity.go
+++ entity/comment.entity.go
@@ -5,21 +5,17 @@ import (
 	"gorm.io/gorm"
 )
 
-type Article struct {
+type Comment struct {
 	ID        string `json:"id" gorm:"primaryKey"`
-	PhotoURL  string `json:"photo_url" gorm:"not null"`
-	Title     string `json:"title" gorm:"not null"`
 	Body      string `json:"body" gorm:"not null"`
-	Category  string `json:"category" gorm:"not null"`
-	Likes     int    `json:"likes" gorm:"not null"`
-	Dislikes  int    `json:"dislikes" gorm:"not null"`
+	ArticleID string `json:"article_id" gorm:"not null"`
 	UserID    string `json:"user_id" gorm:"not null"`
 	CreatedAt int64  `json:"created_at" gorm:"autoCreateTime:milli"`
 	UpdatedAt int64  `json:"updated_at" gorm:"autoUpdateTime:milli"`
 	DeletedAt int64  `json:"deleted_at" gorm:"index"`
 }
 
-func (a *Article) BeforeCreate(tx *gorm.DB) (err error) {
-	a.ID = uuid.New().String()
+func (c *Comment) BeforeCreate(tx *gorm.DB) (err error) {
+	c.ID = uuid.New().String()
 	return
 }
